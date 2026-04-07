da11282798849538f765a82f4ffb9991b567f7db
Goh Kun Shun (KHUN) <kunshun225@gmail.com>
2026-04-08 01:49:39 +0800

    Switch to clean URLs and re-enable @astrojs/sitemap.

--- astro-blog/astro.config.mjs
+++ astro-blog/astro.config.mjs
@@ -1,12 +1,16 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
+import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
   site: 'https://xkhunx.github.io',
-  integrations: [mdx()],
-  build: {
-    // Generates /2020/11/22/who-is-chan-sow-lin.html matching Jekyll's permalink format
-    format: 'file',
+  integrations: [mdx(), sitemap()],
+  redirects: {
+    '/2019/08/18/welcome-to-khuns-blog.html': '/2019/08/18/welcome-to-khuns-blog',
+    '/2019/08/25/the-mystery-of-the-broken-bridge-in-putrajaya.html': '/2019/08/25/the-mystery-of-the-broken-bridge-in-putrajaya',
+    '/2019/09/15/the-creature-with-magical-ballsack.html': '/2019/09/15/the-creature-with-magical-ballsack',
+    '/2020/03/26/machine-learning-for-beginners-using-ai-to-write-simple-functions-like-iseven.html': '/2020/03/26/machine-learning-for-beginners-using-ai-to-write-simple-functions-like-iseven',
+    '/2020/11/22/who-is-chan-sow-lin.html': '/2020/11/22/who-is-chan-sow-lin',
   },
   markdown: {
     shikiConfig: {
